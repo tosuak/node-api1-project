1634675fb20827078b0dfb08c7bc6a03d598c986
Tafiqul <demianviotto@gmail.com>
2022-03-07 19:18:51 -0500

    fixing bugs

--- api/server.js
+++ api/server.js
@@ -27,7 +27,7 @@ server.get('/api/users', (req, res) => {
     .catch(err => {
       res.status(500).json({ message: "The users information could not be retrieved" });
     })
-})
+});
 
 server.get('/api/users/:id', (req, res) => {
   User.findById(req.params.id)
@@ -41,6 +41,39 @@ server.get('/api/users/:id', (req, res) => {
     .catch(err => {
       res.status(500).json({ message: "The user information could not be retrieved" })
     })
-})
+});
+
+server.delete('/api/users/:id', (req, res) => {
+  User.remove(req.params.id)
+    .then(user => {
+      if (user == null) {
+        res.status(404).json({ message: 'The user with the specified ID does not exist' });
+      } else {
+        res.json(user);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'The user could not be removed' });
+    })
+});
+
+server.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, bio } = req.body;
+
+  User.update(id, {name, bio})
+    .then(updatedUser => {
+      if (updatedUser.id == null) {
+        res.status(404).json({ message: 'The user with the specified ID does not exist' });
+      } else if (updatedUser.name == null || updatedUser.bio == null) {
+        res.status(400).json({ message: 'Please provide name and bio for the user'});
+      } else {
+        res.json(updatedUser);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'The user information could not be modified' });
+    })
+});
 
 module.exports = server;
